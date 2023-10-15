63b02365bb64c13e841572ce3b9ed6bc79efe4ce
shadow <lightshadow416@gmail.com>
2023-10-15 20:36:55 +0100

    [ControllPanel] component was created

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
 export { default as MyContainer } from "./MyContainer/MyContainer";
 export { default as CopyField } from "./CopyField/CopyField";
+export { default as ControlPanel } from "./ControlPanel/ControlPanel";
