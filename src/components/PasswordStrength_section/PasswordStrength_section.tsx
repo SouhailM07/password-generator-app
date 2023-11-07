98bff94c6b692dddd0475c607ee2466d80d998f1
shadow <lightshadow416@gmail.com>
2023-11-07 20:30:13 +0100

    [PasswordStrength_section] component classes -> CSS file

--- src/components/PasswordStrength_section/PasswordStrength_section.tsx
+++ src/components/PasswordStrength_section/PasswordStrength_section.tsx
@@ -1,15 +1,17 @@
+// style
+import "./passwordStrength_section.css";
 // ! redux
 import { useSelector } from "react-redux";
 
 export default function PasswordStrength_section() {
   let strength = useSelector((state: any) => state.passwordArray.strength);
 
-  let bars: any = [];
+  let bars: number[] = [];
   for (let i = 0; i < strength; i++) {
     bars.push(i);
   }
   //
-  let bars2: any = [1, 2, 3, 4];
+  let bars2: number[] = [1, 2, 3, 4];
   for (let i = 0; i < strength; i++) {
     bars2.pop();
   }
@@ -18,30 +20,18 @@ export default function PasswordStrength_section() {
 
   return (
     <>
-      <div className="h-[4.5rem] md:h-[4.4vw] w-[100%] bg-screen px-[2rem] flex justify-between items-center">
-        <p className="text-labelsColor text-[1.3rem] md:text-[1.3vw] font-bold">
-          STRENGTH
-        </p>
-        <div className="flex items-center justify-between w-[11.5rem] md:w-[11.5vw]">
-          <p className="uppercase text-[1.4rem] md:text-[1.4vw]">
+      <div id="PasswordStrength">
+        <p>STRENGTH</p>
+        <div>
+          <p id="strength-type" className="">
             {strengthType[strength]}
           </p>
-          <div className="flex justify-between w-[5.1rem] md:w-[5vw]">
+          <div id="bars-container">
             {bars.map((i) => {
-              return (
-                <div
-                  key={i}
-                  className="bg-bars h-[2.3rem] w-[0.8rem] md:h-[2.3vw] md:w-[0.8vw]"
-                ></div>
-              );
+              return <div key={i} className="bars1"></div>;
             })}
             {bars2.map((i) => {
-              return (
-                <div
-                  key={i}
-                  className="bars border-[2px] border-white h-[2.3rem] w-[0.8rem] md:h-[2.3vw] md:w-[0.8vw]"
-                ></div>
-              );
+              return <div key={i} className="bars2 "></div>;
             })}
           </div>
         </div>
