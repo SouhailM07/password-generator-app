84ace5fae84657320215cf6259d3d618de1df1e6
shadow <lightshadow416@gmail.com>
2023-11-07 18:48:07 +0100

    [CheckLines] component classes -> CSS file

--- src/components/CheckLines/CheckLines.tsx
+++ src/components/CheckLines/CheckLines.tsx
@@ -1,5 +1,7 @@
+// style
+import "./checklines.css";
 // hooks
-import { useState, useRef } from "react";
+import { useState } from "react";
 // ! redux
 import { useDispatch } from "react-redux";
 
@@ -7,14 +9,12 @@ export default function CheckLines({ Id, line, checkedF, notCheckedF }) {
   let dispatch = useDispatch();
   //
   let [check, setCheck] = useState(true);
-  let checkRef: any = useRef("");
   return (
     <>
-      <div className=" text-[1.2rem] md:text-[1.2vw] font-bold">
+      <div className="checkLine">
         <input
           type="checkbox"
           id={Id}
-          ref={checkRef}
           // className="mr-[1.5rem] h-[1.3vw] w-[1.3vw] m-[10rem]"
           onChange={() => {
             if (check) {
