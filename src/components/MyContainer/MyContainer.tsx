0e3c6151b7094505d04905a0ffb4ad87215ab1ca
shadow <lightshadow416@gmail.com>
2023-12-04 03:36:00 +0100

    desktop layout was corrected

--- src/components/MyContainer/MyContainer.tsx
+++ src/components/MyContainer/MyContainer.tsx
@@ -3,8 +3,8 @@ import { CopyField, ControlPanel } from "../../components";
 
 export default function MyContainer() {
   return (
-    <main className="text-white flex flex-col items-center justify-between  h-[42rem] md:h-[42vw] w-[37rem] md:w-[36vw]">
-      <h1 className="text-[1.5rem] md:text-[1.5vw] font-medium text-labelsColor">
+    <main className="text-white flex flex-col items-center justify-between  h-[42rem] w-[37rem] border-2">
+      <h1 className="text-[1.5rem] font-medium text-labelsColor">
         Password Generator
       </h1>
       <CopyField />
